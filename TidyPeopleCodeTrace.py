482bbdcf1b9ee2041045d6eb90af7d52b31a2f95
Jay Mathew <jay.mathew@gmail.com>
2014-04-29 15:50:55 +1000

    Update TidyPeopleCodeTrace.py
    Found a more efficient regex to fix up unmatched quotes

--- TidyPeopleCodeTrace.py
+++ TidyPeopleCodeTrace.py
@@ -12,30 +12,18 @@ class TidypctraceCommand(sublime_plugin.TextCommand):
         alltextreg = sublime.Region(0, view.size())
         lines = view.lines(alltextreg)
         allLines = ''
-        
-        for line in lines:
-            lineContents = view.substr(line)
 
-            ## Remove header timings
-            match = re.search(r'(^PSAPPSRV.*?\d\.\d{6}\s)(.*)', lineContents)
-            if match:
-                    lineContents = match.group(2)
-
-            ## Find unmatched quotes and add them
-            quoteCount = 0
-            for char in lineContents:
-                if char == '"':
-                    quoteCount = quoteCount + 1
-
-            if (quoteCount % 2) != 0:
-                lineContents = lineContents + '"'
-                
-            allLines = allLines + lineContents + '\n'                        
-        
-        view.replace(edit, alltextreg, allLines)
-
+        ## Fix up unmatched quotes  
         extractions = []
+        regions = regex_findall(self, find='(.*)("[^";\s>]+$)', flags=0, replace='\\1\\2"', extractions=extractions)
+        greedy_replace(self, extractions, regions)
+
+        ## Remove header junk
+        extractions = []
+        regions = regex_findall(self, find='(^PSAPPSRV.*?\d\.\d{6}\s)(.*)', flags=0, replace='\\2', extractions=extractions)
+        greedy_replace(self, extractions, regions)        
         
-        ## Remove all blank spaces       
+        ## Remove all blank spaces
+        extractions = []
         regions = regex_findall(self, find='^\n', flags=0, replace='', extractions=extractions)
         greedy_replace(self, extractions, regions)
