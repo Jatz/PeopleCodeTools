3b02eaa1212b6e0612c3d22f7418f1fb07ad9f48
Jay Mathew <jay.mathew@gmail.com>
2014-05-02 14:49:47 +1000

    Update TidyPeopleCodeTrace.py

--- TidyPeopleCodeTrace.py
+++ TidyPeopleCodeTrace.py
@@ -10,38 +10,47 @@ class TidypctraceCommand(sublime_plugin.TextCommand):
     def run(self, edit):
         settings = sublime.load_settings(SETTINGS_FILE)
         self.edit = edit
-        view = self.view
-        regions = view.sel()
-        alltextreg = sublime.Region(0, view.size())
-        lines = view.lines(alltextreg)
-        allLines = ''
+
+        # Grab the current view contents and store it in a new file
+        currentView = self.view;
+        currentViewContent = currentView.substr(sublime.Region(0, currentView.size()))        
+        newView = currentView.window().new_file()
+        newViewAllTextRegion = sublime.Region(0, newView.size())
+        newView.replace(edit, newViewAllTextRegion, currentViewContent)
+
+        # Get location of syntax file
+        currentSyntax = currentView.settings().get('syntax')
 
         ## Remove header junk
         if settings.get("tidy_remove_psappsrv_headers") == True:
             extractions = []
-            regions = regex_findall(self, find='(^PSAPPSRV.*?\d\.\d{6}\s)(.*)', flags=0, replace='\\2', extractions=extractions)
-            greedy_replace(self, extractions, regions)
+            regions = regex_findall(newView, find='(^PSAPPSRV.*?\d\.\d{6}\s)(.*)', flags=0, replace='\\2', extractions=extractions)
+            greedy_replace(self, newView, extractions, regions)
 
             extractions = []
-            regions = regex_findall(self, find='(^PSAPPSRV.*@JavaClient.*IntegrationSvc\]\(\d\)\s{3})(.*)', flags=0, replace='\\2', extractions=extractions)
-            greedy_replace(self, extractions, regions)
+            regions = regex_findall(newView, find='(^PSAPPSRV.*@JavaClient.*IntegrationSvc\]\(\d\)\s{3})(.*)', flags=0, replace='\\2', extractions=extractions)
+            greedy_replace(self, newView, extractions, regions)
 
         ## Fix up unmatched quotes
         if settings.get("tidy_add_unmatched_quotes") == True:
             extractions = []
-            regions = regex_findall(self, find='(.*"[^";\)\s>]+$)', flags=0, replace='\\1" - quote added by Tidy', extractions=extractions)
-            greedy_replace(self, extractions, regions)
+            regions = regex_findall(newView, find='(.*"[^";\)\s>]+$)', flags=0, replace='\\1" - quote added by Tidy', extractions=extractions)
+            greedy_replace(self, newView, extractions, regions)
 
             extractions = []
-            regions = regex_findall(self, find='(.*\("[^"]+$)', flags=0, replace='\\1" - quote added by Tidy', extractions=extractions)
-            greedy_replace(self, extractions, regions)
+            regions = regex_findall(newView, find='(.*\("[^"]+$)', flags=0, replace='\\1" - quote added by Tidy', extractions=extractions)
+            greedy_replace(self, newView, extractions, regions)
 
             extractions = []
-            regions = regex_findall(self, find='(.*="$)', flags=0, replace='\\1" - quote added by Tidy', extractions=extractions)
-            greedy_replace(self, extractions, regions)    
+            regions = regex_findall(newView, find='(.*="$)', flags=0, replace='\\1" - quote added by Tidy', extractions=extractions)
+            greedy_replace(self, newView, extractions, regions)    
         
         ## Remove all blank spaces
         if settings.get("tidy_remove_blank_spaces") == True:
             extractions = []
-            regions = regex_findall(self, find='^\n', flags=0, replace='', extractions=extractions)
-            greedy_replace(self, extractions, regions)
+            regions = regex_findall(newView, find='^\n', flags=0, replace='', extractions=extractions)
+            greedy_replace(self, newView, extractions, regions)
+
+        # Set syntax of new file and clear current selection
+        newView.set_syntax_file(currentSyntax)
+        newView.sel().clear()
