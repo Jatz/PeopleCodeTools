71d6eecb9ebf94acc695ff78b290b2d746d17514
Jay Mathew <jay.mathew@gmail.com>
2014-05-09 16:12:35 +1000

    Update ExtractCallStack.py
    Rewrote tool to improve efficiency

--- ExtractCallStack.py
+++ ExtractCallStack.py
@@ -1,61 +1,54 @@
 import sublime, sublime_plugin, re
-from .libs.regex import regex_extract
-from .libs.regex import regex_findall
-from .libs.regex import greedy_replace
 
 class ExtractpccallstackCommand(sublime_plugin.TextCommand):
 
-    def replaceCurrentViewContent(self, currentView, replaceString):
-        currentViewAllTextRegion = sublime.Region(0, currentView.size())
-        currentView.replace(self.edit, currentViewAllTextRegion, replaceString)
-        currentView.sel().clear()
+    # This is only used for debugging
+    def replaceViewContent(self, viewToReplace, replaceString):
+        viewToReplaceAllTextRegion = sublime.Region(0, viewToReplace.size())
+        viewToReplace.replace(self.edit, viewToReplaceAllTextRegion, replaceString)
+        viewToReplace.sel().clear()
         
     def run(self, edit):
         self.edit = edit
 
         # Grab the current view contents and store it in a new file
-        currentView = self.view;
+        originalView = self.view;
 
-        # Take a copy of the current view contents
-        # Unfortunately if I copy the contents immediately to a new window and then perform the regex operations on it
-        # the greedy_replace function will crash Sublime - not sure whether this is a bug with Sublime.
-        currentViewContent = currentView.substr(sublime.Region(0, currentView.size()))
-        newView = currentView.window().new_file()
-        
-        extractions = []
-
+        originalViewContent = originalView.substr(sublime.Region(0, originalView.size()))
+        workingView = originalView.window().new_file()             
+    
         # First remove all lines that have a call method followed by a start-ext, since the start-ext is sufficient
         # For example: the following call method line will be ignored since there is a start-ext immediately after it:
-        # PSAPPSRV.4556 (2426) 	 1-8760   14.05.07    0.000000       call method  SSF_CFS:SSF_CFQKey.SSFQKeyString #params=7
-        # PSAPPSRV.4556 (2426) 	 1-8761   14.05.07    0.000000   >>> start-ext Nest=01 SSFQKeyString SSF_CFS.SSF_CFQKey.OnExecute       
-        regions = regex_findall(currentView, find='(.*call method.*\n)(?=.*start-ext)', flags=0, replace='', extractions=extractions)
-        greedy_replace(self, currentView, extractions, regions) 
-       
+        # PSAPPSRV.4556 (2426)   1-8760   14.05.07    0.000000       call method  SSF_CFS:SSF_CFQKey.SSFQKeyString #params=7
+        # PSAPPSRV.4556 (2426)   1-8761   14.05.07    0.000000   >>> start-ext Nest=01 SSFQKeyString SSF_CFS.SSF_CFQKey.OnExecute
+        workingViewString = re.sub(r'(.*call method.*\n)(?=.*start-ext)', '', originalViewContent)
+        
         # Extract all lines containing start, end, Nest=, call int, call private, call method and End-Function        
-        regex_extract(self, currentView, r'((.*(start|end|resume|reend).*Nest=.*)|(.*call (int|private|method).*)|.*End-Function.*)')
-
-        # Get the unique session numbers            
-        sessionNos = []
-        regions = regex_findall(currentView, find='PSAPPSRV.\d+\s+\((\d+)\).*', flags=0, replace='\\1', extractions=sessionNos)
+        str_list = re.findall(r'(?:(?:.*(?:start|end|resume|reend).*Nest=.*)|(?:.*call (?:int|private|method).*)|.*End-Function.*)', workingViewString, re.MULTILINE)
+        workingViewString = '\n'.join(str_list)
+        
+        # Get the unique session numbers and store them in a list
+        sessionNos = re.findall(r'PSAPPSRV.\d+\s+\((\d+)\)', workingViewString, re.MULTILINE)
         sessionNos = sorted(set(sessionNos))
 
-        extractBackup = currentView.substr(sublime.Region(0, currentView.size()))
-
         sessionCount = 1
+        callStack = ''
         for sessionNo in sessionNos:
-            self.replaceCurrentViewContent(currentView, extractBackup)            
-            regex_extract(self, currentView, r'PSAPPSRV.\d+\s+\((%s)\).*' % sessionNo)
-            regex_extract(self, currentView, r'(((start|end|resume|reend).*Nest=.*)|(call (int|private|method).*)|End-Function.*)')
-       
-            alltextreg = sublime.Region(0, currentView.size())            
-            allLines = ''
-            lines = currentView.lines(alltextreg)            
+
+            #regex_extract(self, workingView, r'PSAPPSRV.\d+\s+\((%s)\).*' % sessionNo)
+            str_list = re.findall(r'PSAPPSRV\.\d+\s+\(%s\).*' % sessionNo, workingViewString, re.MULTILINE)
+            sessionSpecificString = '\n'.join(str_list)
+
+            #regex_extract(self, workingView, r'(((start|end|resume|reend).*Nest=.*)|(call (int|private|method).*)|End-Function.*)')
+            str_list = re.findall(r'(?:(?:(?:start|end|resume|reend).*Nest=.*)|(?:call (?:int|private|method).*)|End-Function.*)', sessionSpecificString, re.MULTILINE)
+            sessionSpecificString = '\n'.join(str_list)
+            lines = sessionSpecificString.split('\n')
+            sessionSpecificString = ''                    
             lastCall = ''
             nestLevel = 0
         
             # Perform initial formatting based on Nest value
-            for line in lines:
-                lineContents = currentView.substr(line)
+            for lineContents in lines:
                 # extract Nest value from lineContents
                 match = re.search(r'(start|end|resume|reend).*Nest=(\d+)', lineContents)
                 if match:                                       
@@ -65,7 +58,7 @@ class ExtractpccallstackCommand(sublime_plugin.TextCommand):
                     
                     for x in range(startIndex,nestLevel):
                         lineContents = '\t' + lineContents
-                    allLines = allLines + lineContents + '\n'                    
+                    sessionSpecificString = sessionSpecificString + lineContents + '\n'                    
 
                     if match.group(1) == 'start':
                         lastCall = 'start'
@@ -93,44 +86,26 @@ class ExtractpccallstackCommand(sublime_plugin.TextCommand):
                         lineContents = '\t' + lineContents
 
                     if lastCall == 'start':
-                        allLines = allLines + '\t' + lineContents + '\n'
+                        sessionSpecificString = sessionSpecificString + '\t' + lineContents + '\n'
                     else:
-                        allLines = allLines + lineContents + '\n'
+                        sessionSpecificString = sessionSpecificString + lineContents + '\n'
                         
                     if lastCall == 'callInt':
                         nestLevel = nestLevel + 1
-                
-            currentView.replace(edit, alltextreg, allLines)
 
             # Remove nest from the header
-            extractions = []
-            regions = regex_findall(currentView, find='(?<=(start)).*Nest=\d\d', flags=0, replace='', extractions=extractions)
-            greedy_replace(self, currentView, extractions, regions)
-
-            extractions = []
-            regions = regex_findall(currentView, find='(?<=(reend)).*Nest=\d\d\s\.', flags=0, replace='', extractions=extractions)
-            greedy_replace(self, currentView, extractions, regions)    
-
-            extractions = []
-            regions = regex_findall(currentView, find='(?<=(end)).*Nest=\d\d', flags=0, replace='', extractions=extractions)
-            greedy_replace(self, currentView, extractions, regions)
-
-            extractions = []
-            regions = regex_findall(currentView, find='(?<=(resume)).*Nest=\d\d\s\.', flags=0, replace='', extractions=extractions)
-            greedy_replace(self, currentView, extractions, regions)   
+            sessionSpecificString = re.sub(r'\s+Nest=\d+', '', sessionSpecificString)
 
             # Are there any resume or reend statements?
-            # If so, then reformat the entire trace based on the resume and reend statements
-            extractions = []
-            regions = regex_findall(currentView, find='^(resume|reend)\s(.*)', flags=0, replace='', extractions=extractions)
-            if regions:
+            # If so, then reformat the session specific string based on the resume and reend statements
+            found = re.search('(resume|reend)\s(.*)', sessionSpecificString)
+            if found:
                 # Store remaining text line by line in a dict called results
-                alltextreg = sublime.Region(0, currentView.size())
-                lines = currentView.lines(alltextreg)
+                lines = sessionSpecificString.split('\n')
                 results = {}
                 lineNo = 1
-                for line in lines:
-                    results[lineNo] = currentView.substr(line)
+                for lineContents in lines:
+                    results[lineNo] = lineContents
                     lineNo = lineNo + 1
 
                 resumeResults = {}
@@ -164,45 +139,34 @@ class ExtractpccallstackCommand(sublime_plugin.TextCommand):
                             if x != reendResultLineNo:
                                 results[x] = '\t' + results[x]                        
 
-                allLines = ''
+                sessionSpecificString = ''
                 for lineNo, line in results.items():
-                    allLines = allLines + results[lineNo] + '\n'
-                    
-                currentView.replace(edit, alltextreg, allLines)
+                    sessionSpecificString = sessionSpecificString + results[lineNo] + '\n'
+                                    
 
             # Clean lines
             # Remove the end-ext, End-Function, resume, end and reend calls, since we no longer need them
-            regex_extract(self, currentView, r'(.*(start).*)|.*(call (int|private|method).*)')
+            str_list = re.findall(r'(?:.*(?:start).*)|.*(?:call (?:int|private|method).*)', sessionSpecificString, re.MULTILINE)
+            sessionSpecificString = '\n'.join(str_list)           
 
             # Remove the start and call int/private/method strings from all lines as these strings are no longer required
-            extractions = []
-            regions = regex_findall(currentView, find='(start|call (int|private|method))\s+', flags=0, replace='', extractions=extractions)
-            greedy_replace(self, currentView, extractions, regions)
-
+            sessionSpecificString = re.sub(r'(start|call (int|private|method)|start-ext)\s+', '', sessionSpecificString)
+            
             # Remove unnecessary trailer junk (e.g. params= or #params=)
-            extractions = []
-            regions = regex_findall(currentView, find='Dur=.*', flags=0, replace='', extractions=extractions)
-            greedy_replace(self, currentView, extractions, regions)
+            sessionSpecificString = re.sub(r'Dur=.*', '', sessionSpecificString)
+            sessionSpecificString = re.sub(r'[\s]#?params=\d+', '', sessionSpecificString)
 
-            extractions = []
-            regions = regex_findall(currentView, find='[\s]#?params=\d+', flags=0, replace='', extractions=extractions)
-            greedy_replace(self, currentView, extractions, regions)       
-
-            # We now have the complete callstack for the session so store it in a string
-            callStack = currentView.substr(sublime.Region(0, currentView.size()))
+            # We now have the complete callstack for the session so store it in a string called callStack
+            callStack = sessionSpecificString
 
             # Insert the call stack in the new view
-            newViewAllTextRegion = sublime.Region(0, newView.size())
+            workingViewAllTextRegion = sublime.Region(0, workingView.size())
             if sessionCount == 1:
-                newView.insert(edit, newViewAllTextRegion.end(), 'Session %s:\n' % sessionNo + callStack)
+                workingView.insert(edit, workingViewAllTextRegion.end(), 'Session %s:\n' % sessionNo + callStack)
             else:
-                newView.insert(edit, newViewAllTextRegion.end(), '\nSession %s:\n' % sessionNo + callStack)
-            newView.sel().clear()
+                workingView.insert(edit, workingViewAllTextRegion.end(), '\nSession %s:\n' % sessionNo + callStack)
+            workingView.sel().clear()
 
             # Increment session count, go to next sessionNo if available
             sessionCount += 1
-
-        # Restore the current view to its original contents
-        currentViewAllTextRegion = sublime.Region(0, currentView.size())
-        currentView.replace(edit, currentViewAllTextRegion, currentViewContent)
-        currentView.sel().clear()
+        
